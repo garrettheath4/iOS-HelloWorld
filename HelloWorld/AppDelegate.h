e6b0da3e4d40178f84faef2bad2bdc5fdd08c5a6
Garrett Heath Koller <garrettheath4@gmail.com>
2012-04-24 14:59:22 -0400

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Garrett Koller on 4/24/12.
+//  Copyright (c) 2012 Washington and Lee University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Garrett Koller on 4/24/12.
+//  Copyright (c) 2012 Washington and Lee University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
